da4adec1c77058f4259098fb7f3942de231e03fb
albertgoncalves <goncalves.albert@gmail.com>
2022-10-17 21:31:27 -0400

    add `mypaint` to dev env

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 mkShell.override { stdenv = llvmPackages_14.stdenv; } {
     buildInputs = [
         html-tidy
+        mypaint
         nodePackages.jshint
         python3Packages.flake8
         shellcheck
